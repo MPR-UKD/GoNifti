388abc41f2723243370662cbdcf54fe619d5a177
ludgerradke <radke@intervall.io>
2023-02-12 20:39:18 +0100

    feat: Implement dicom_to_nifti function
    The `dicom_to_nifti` function takes a folder containing DICOM images and converts them into a 4D Nifti array. The organizing parameter is automatically detected, and all relevant information from the DICOM header is transferred to the Nifti header.

--- utils.py
+++ utils.py
@@ -2,6 +2,10 @@ import os
 from pathlib import Path
 from typing import List
 
+import nibabel as nib
+import numpy as np
+import pydicom
+
 
 def find_dicom_folders(root_folder: Path) -> List[Path]:
     dicom_folders = []
@@ -15,3 +19,42 @@ def find_dicom_folders(root_folder: Path) -> List[Path]:
         if has_dicom_files:
             dicom_folders.append(root)
     return dicom_folders
+
+
+def dicom_to_nifti(folder: Path, verbose: bool = False) -> nib.Nifti1Image:
+    # Load all DICOM images in the folder
+    dicom_files = [pydicom.dcmread(str(file)) for file in folder.glob('*.dcm')]
+
+    # Determine the changing parameter
+    changing_parameters = set()
+    for d in dicom_files:
+        changing_parameters |= set(d.keys())
+    changing_parameters -= set(dicom_files[0].keys())
+
+    if verbose:
+        print(f"Changed parameters detected: {changing_parameters}")
+    # Use the first changing parameter found as the organizing parameter
+    organizing_parameter = list(changing_parameters)[0] if changing_parameters else None
+
+    # Organize the DICOM images based on the organizing parameter if found
+    if organizing_parameter:
+        dicom_files.sort(key=lambda x: x.get(organizing_parameter, 0))
+        if verbose:
+            print(f"Organizing parameter: {organizing_parameter}")
+
+    # Convert the DICOM images to a 4D Nifti array
+    image_data = np.stack([d.pixel_array for d in dicom_files])
+    affine = dicom_files[0].affine
+
+    header = nib.Nifti1Header()
+    # Transfer DICOM header information to Nifti header
+    for key, value in dicom_files[0].items():
+        if key not in header:
+            try:
+                header[key] = value
+            except:
+                pass
+
+    nifti_img = nib.Nifti1Image(image_data, affine, header)
+
+    return nifti_img

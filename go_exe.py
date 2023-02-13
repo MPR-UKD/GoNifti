fd2ab79c30ccd1e19970aaea4e84e224139d1c91
ludgerradke <radke@intervall.io>
2023-02-13 08:30:14 +0100

    Refactoring

--- go_exe.py
+++ go_exe.py
@@ -7,9 +7,9 @@ if os.path.exists(r"./build"):
 if os.path.exists(r"./dist"):
     shutil.rmtree(r"./dist")
 os.system(
-    "pyinstaller --collect-submodules=pydicom go_nifti.py --onedir"
+    "pyinstaller --collect-submodules=pydicom GoNiftiGUI.py --onedir"
 )
 shutil.make_archive("./dist/DICOMTranslator", "zip", "./dist/go_nifti")
 os.system(
-    "pyinstaller --collect-submodules=pydicom go_nifti.py --onefile"
+    "pyinstaller --collect-submodules=pydicom GoNiftiGUI.py --onefile"
 )

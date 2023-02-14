b46172df3dc5b6ad9987170f2963ae2451bd19dc
ludgerradke <radke@intervall.io>
2023-02-14 08:56:59 +0100

    Exe files updated.

--- go_exe.py
+++ go_exe.py
@@ -9,12 +9,14 @@ if os.path.exists(r"./dist"):
 os.system(
     "pyinstaller --hidden-import=PyQt5 --collect-submodules=pydicom GoNiftiGUI.py --onedir"
 )
+shutil.make_archive("./dist/GoNiftiGUI", "zip", "./dist/GoNiftiGUI")
 os.system(
     "pyinstaller --hidden-import=PyQt5 --collect-submodules=pydicom GoNiftiGUI.py --onefile"
 )
 os.system(
     "pyinstaller --hidden-import=PyQt5 --collect-submodules=pydicom GoNifti.py --onedir"
 )
+shutil.make_archive("./dist/GoNifti", "zip", "./dist/GoNifti")
 os.system(
     "pyinstaller --hidden-import=PyQt5 --collect-submodules=pydicom GoNifti.py --onefile"
 )

af4f44dc76202da65c46fe7b5654296d1c43305e
ludgerradke <radke@intervall.io>
2023-02-15 15:11:33 +0100

    test updated

--- test_gonifti.py
+++ test_gonifti.py
@@ -1,43 +1,21 @@
-from go_nifti.src.utils import find_dicom_folders
+import os
+import shutil
 
+import pydicom
+from go_nifti.src.GoNifti import convert
+from pydicom.data import get_testdata_files
+from pathlib import Path
 
 def test_find_dicom_folders(tmp_path):
     # create test directory structure
     root = tmp_path / "test"
     root.mkdir()
-    subdirs = [
-        "subdir1",
-        "subdir2",
-        "subdir3",
-        "subdir4",
-        "subdir5",
-        "subdir6",
-        "subdir7",
-    ]
-    for subdir in subdirs:
-        (root / subdir).mkdir()
-        with open(root / subdir / "file.txt", "w") as f:
-            f.write("test")
 
-    # create dicom files in test subdirectories
-    dicom_files = [
-        "image1.dcm",
-        "image2.dcm",
-        "image3.dcm",
-        "image4.dcm",
-        "image5.dcm",
-        "image6.dcm",
-    ]
-    for i, subdir in enumerate(subdirs[:6]):
-        dicom_dir = root / subdir / f"dicom{i+1}"
-        dicom_dir.mkdir()
-        for dicom_file in dicom_files:
-            with open(dicom_dir / dicom_file, "w") as f:
-                f.write("test")
+    # load a DICOM file from pydicom test data and save it in the test directory
+    for dcm_file in get_testdata_files("CT_small.dcm"):
+        shutil.copy(dcm_file, root / Path(dcm_file).name)
 
-    # find dicom folders
-    dicom_folders = find_dicom_folders(root)
+    # call the conversion function with the test directory
+    convert(root_folder=root, mode='save_in_separate_dir')
+    assert root.with_suffix('_as_nifti').exists() == True
 
-    # check that correct folders are found
-    expected_folders = [root / subdir / f"dicom{i+1}" for i, subdir in enumerate(subdirs[:6])]
-    assert dicom_folders == expected_folders

a9bd86fb5ffeca4ff4af11099ec2b34b35a2f0bc
ludgerradke <radke@intervall.io>
2023-02-15 17:16:23 +0100

    test updated

--- test_gonifti.py
+++ test_gonifti.py
@@ -1,21 +1,20 @@
-import os
 import shutil
-
-import pydicom
-from go_nifti.src.GoNifti import convert
-from pydicom.data import get_testdata_files
 from pathlib import Path
+
+from pydicom.data import get_testdata_files
+
+from go_nifti.src.GoNifti import convert
+
 
 def test_find_dicom_folders(tmp_path):
     # create test directory structure
-    root = tmp_path / "test"
-    root.mkdir()
+    root = tmp_path / "test" / "dicom_dir"
+    root.mkdir(parents=True)
 
     # load a DICOM file from pydicom test data and save it in the test directory
     for dcm_file in get_testdata_files("CT_small.dcm"):
         shutil.copy(dcm_file, root / Path(dcm_file).name)
 
     # call the conversion function with the test directory
-    convert(root_folder=root, mode='save_in_separate_dir')
-    assert root.with_suffix('_as_nifti').exists() == True
-
+    convert(root_folder=root.parent, mode='save_in_separate_dir')
+    assert (root.parent / (root.parent.name + '_as_nifti')).exists()(root.parent.parent / (root.parent.name + '_as_nifti')).exists() == True

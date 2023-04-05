a1ade38108d20e539d8ab579f5daafeb490bc82f
ludgerradke <radke@intervall.io>
2023-04-05 11:45:02 +0100

    update exe

--- go_nifti/src/GoNifti.py
+++ go_nifti/src/GoNifti.py
@@ -5,8 +5,8 @@ from pathlib import Path
 import click
 import dicom2nifti
 
-from .utils import find_dicom_folders, dicom_to_nifti, save_nifti
-from .utils import nifti_change_dtype
+from go_nifti.src.utils import find_dicom_folders, dicom_to_nifti, save_nifti
+from go_nifti.src.utils import nifti_change_dtype
 import multiprocessing
 
 
